48d82a0c3aa75897ee384ccee8b2d8728d836a59
tunip3 <ts-gaming@outlook.com>
2018-06-10 12:10:15 +0100

    Update addon.py

--- addon.py
+++ addon.py
@@ -1,11 +1,13 @@
 import xbmcaddon
 import xbmcgui
- 
+import os
+
 addon       = xbmcaddon.Addon()
 addonname   = addon.getAddonInfo('name')
  
 line1 = "Hello World!"
 line2 = "We can write anything we want here"
 line3 = "Using Python"
- 
-xbmcgui.Dialog().ok(addonname, line1, line2, line3)
+line4 = os.getcwd()
+
+xbmcgui.Dialog().ok(addonname, line1, line2, line3, line4)
