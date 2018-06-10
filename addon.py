edf89f090f9a49f6b99d4d84b302a6d2707a7927
tunip3 <ts-gaming@outlook.com>
2018-06-10 12:17:17 +0100

    Update addon.py

--- addon.py
+++ addon.py
@@ -8,6 +8,6 @@ addonname   = addon.getAddonInfo('name')
 line1 = "Hello World!"
 line2 = "We can write anything we want here"
 line3 = "Using Python"
-line4 = os.getcwd()
+line4 = "derp derp"
 
 xbmcgui.Dialog().ok(addonname, line1, line2, line3, line4)
